462a4533dd9e7e196819654895590b4ad803d773
levyitay <levyitay@gmail.com>
2011-01-06 19:24:38 +0200

    some changes in xmlBuilder

--- src/com/Rest2Go/Utils/xmlBuilder.java
+++ src/com/Rest2Go/Utils/xmlBuilder.java
@@ -10,22 +10,20 @@ import java.sql.SQLException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+
+
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class xmlBuilder {
 	
-	public static Document buildXMLFromString (String inputString) throws ParserConfigurationException, SAXException, IOException
+	public static Document buildXMLFromString (String inputString) throws Exception
 	{
 		DocumentBuilderFactory dbf =
             DocumentBuilderFactory.newInstance();
@@ -51,39 +49,35 @@ public class xmlBuilder {
 		 catch (ParserConfigurationException parserException) {
 			 parserException.printStackTrace();
 		 }
-		 Element root = document.createElement("USERS");
+		 Element root = document.createElement("RESTAURANTS");
 		 document.appendChild(root);
 		 
 		 
 			 int i=0;
 				while (resSet.next())
 				{
-					Element User = document.createElement("USER");
+					Element User = document.createElement("Restaurant");
 					root.appendChild(User);
 					
-					Element FirstName = document.createElement("FIRSTNAME");
-					String fn = resSet.getString("First_Name");
-					FirstName.appendChild(document.createTextNode(fn));
-					User.appendChild(FirstName);
+					Element Name = document.createElement("NAME");
 					
-					Element Last_Name = document.createElement("LASTNAME");
-					Last_Name.appendChild(document.createTextNode(resSet.getString("Last_Name")));
-					User.appendChild(Last_Name);
+					Name.appendChild(document.createTextNode(resSet.getString("Name")));
+					User.appendChild(Name);
+					
 					
 					Element Address = document.createElement("ADDRESS");
 					Address.appendChild(document.createTextNode(resSet.getString("Address")));
 					User.appendChild(Address);
 					
-					Element City = document.createElement("CITY");
-					City.appendChild(document.createTextNode(resSet.getString("City")));
-					User.appendChild(City);
+					Element Phone = document.createElement("PHONE");
+					Phone.appendChild(document.createTextNode(resSet.getString("Phone")));
+					User.appendChild(Phone);
 					
-					Element Country = document.createElement("COUNTRY");
-					Country.appendChild(document.createTextNode(resSet.getString("Country")));
-					User.appendChild(Country);
+					Element Phone2 = document.createElement("PHONE2");
+					Phone2.appendChild(document.createTextNode(resSet.getString("SecondaryPhone")));
+					User.appendChild(Phone2);
 					
-					Element Birth_Date = document.createElement("BIRTH_DATE");
-					Birth_Date.appendChild(document.createTextNode(resSet.getDate("Birth_Date").toString()));
+					
 					
 					i++;
 				}
@@ -93,30 +87,10 @@ public class xmlBuilder {
 	
 	public static String DocumentToString(Document doc)
 	{
-		StringWriter sw;
-		StreamResult result;
 
-		DOMSource XMLSource = null;
-		Transformer transformer = null;
-
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		try {
-			transformer = transformerFactory.newTransformer();
-		} catch (TransformerConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		sw = new StringWriter();
-		result =  new StreamResult(sw);
-		XMLSource = new DOMSource(doc);
-		
-		try {
-			transformer.transform(XMLSource, result);
-		} catch (TransformerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result.toString();
+		DOMImplementationLS domImplementation = (DOMImplementationLS) doc.getImplementation();
+	    LSSerializer lsSerializer = domImplementation.createLSSerializer();
+	    return lsSerializer.writeToString(doc);   
 	}
 	
 
